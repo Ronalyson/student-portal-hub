38624456c0a17f0e3c146f358e3ffddb8ac0b257
Ronalyson Medeiros <ronalysongba1@gmail.com>
2026-02-03 23:02:24 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+﻿const toggle = document.querySelector(".nav__toggle");
+const nav = document.querySelector(".nav");
+
+if (toggle && nav) {
+  toggle.addEventListener("click", () => {
+    nav.classList.toggle("is-open");
+  });
+
+  nav.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", () => {
+      nav.classList.remove("is-open");
+    });
+  });
+}
+
